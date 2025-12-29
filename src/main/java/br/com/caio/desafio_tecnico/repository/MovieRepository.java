38241b75b23625f94660712db7a20ec94a074aba
Caio Henrique <caioaranha@hotmail.com>
2025-12-29 20:58:56 -0300

    perf: otimização com a anotação EntityGraph

--- src/main/java/br/com/caio/desafio_tecnico/repository/MovieRepository.java
+++ src/main/java/br/com/caio/desafio_tecnico/repository/MovieRepository.java
@@ -2,12 +2,14 @@ package br.com.caio.desafio_tecnico.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.caio.desafio_tecnico.domain.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
+    @EntityGraph(attributePaths = "producers")
     List<Movie> findAllByWinnerTrueOrderByYearAsc();
 
 }
